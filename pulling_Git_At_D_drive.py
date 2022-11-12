fa66d0f7e61d24f2566bba269f492e7e249ed70b
root <root@DESKTOP-Q1OD7LN.localdomain>
2022-11-12 11:39:30 +0900

    feat: add 13 ledpin blink func

---
+++ pulling_Git_At_D_drive.py
@@ -0,0 +1,15 @@
+import os
+
+d_dir = "/mnt/d/morseChat/"
+
+current_path = os.popen("pwd").read()
+
+os.system("cd /mnt/d/")
+
+if os.popen("ls /mnt/d/ | grep morseChat | wc -l").read()[:-1] == "0":
+    os.system("git -C /mnt/d clone https://github.com/ineiw/morseChat.git")
+
+commandLine = "git -C /mnt/d/morseChat pull origin ineiw;" 
+commandLine += "cd " + current_path 
+
+os.system(commandLine)
